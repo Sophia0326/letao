19b30cafc573cb503e7bde2123f447fe8c4a6e51
Sophia <418452876@qq.com>
2017-11-09 21:55:08 +0800

    登录页判断完成,其他页面相同部分完成

--- public/back/js/index.js
+++ public/back/js/index.js
@@ -74,27 +74,4 @@ $(function () {
         }]
     };
     myCharts.setOption(option1);
-    // 点击菜单按钮,左侧边栏收起
-    $('.btn_menu').on('click', function () {
-        $('.lt_aside').toggleClass('hidden_l');
-        $('.rt_aside').toggleClass('hidden_r');
-    })
-    // 点击分类管理,显示隐藏子分类
-    $('.child').prev().on('click', function () {
-        $(this).next().slideToggle();
-    })
-
-    // 弹出框提示是否退出首页
-    $('.btn_confirm').on('click', function () {
-        $.ajax({
-            url: '/employee/employeeLogout',
-            success: function (data) {
-                // 判断success为true的时候,跳转到登录页
-                if (data.success) {
-                    location.href = "login.html";
-                }
-            }
-        })
-    })
-
 })
